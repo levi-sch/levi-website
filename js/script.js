24e410ac51791edf92b036b96e7e60293afea42f
levi-sch <levi-sch@users.noreply.github.com>
2025-02-23 13:58:25 +0100

    Test lottie animatie, want wil geen lokale server doen via python

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Laad de Lottie-animatie
+    var anim = lottie.loadAnimation({
+      container: document.getElementById('lottie'),
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: 'js/logo_animatie_01.json' // Zorg dat dit bestand in de juiste map staat
+    });
+  
+    // Na 6 seconden verwijderen we het splashscreen
+    setTimeout(function() {
+      // Optie 1: Splashscreen element verwijderen
+      var splash = document.getElementById('splash');
+      splash.parentNode.removeChild(splash);
+  
+      // Optie 2: Splashscreen verbergen (gebruik eventueel een fade-out effect via CSS)
+      // splash.style.display = 'none';
+    }, 6000);
+  });
+  
